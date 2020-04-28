095450248da33cf624914e904db0bf1c2e654bd1
kideveloper612 <developer61510@gmail.com>
2020-04-28 15:27:07 +0800

    minor change

--- px_scraper.py
+++ px_scraper.py
@@ -9,14 +9,20 @@ def send_request(url):
     s = requests.Session()
     r_get = s.get(url).text
     __VIEWSTATE, argument_list = parse_html(bs(r_get, 'html5lib'))
+    if not argument_list:
+        return r_get
     index = -1
     while True:
         index += 1
         if len(argument_list) == index:
             result = r_post
             break
-        __EVENTARGUMENT = argument_list[index].replace('\\\\', '\\')
-        print('Folder Count: ', index, 'Total Folders: ', len(argument_list))
+        position_list = [m.start() for m in re.finditer(r'(?=\\)', argument_list[index])]
+        if position_list:
+            __EVENTARGUMENT = argument_list[index][:position_list[0]] + '\\' + argument_list[index][position_list[-1]+1:]
+        else:
+            __EVENTARGUMENT = argument_list[index]
+        print('Folder Index: ', index, ', Total Folders: ', len(argument_list), ', Current Folder: ', __EVENTARGUMENT)
         payload = {'__EVENTTARGET': 'ctl00$ContentPlaceHolderMain$TableOfContent1$TableOfContent1$MenuNavigationTree',
                    '__VIEWSTATE': __VIEWSTATE,
                    '__EVENTARGUMENT': __EVENTARGUMENT
@@ -25,7 +31,7 @@ def send_request(url):
         __VIEWSTATE, tree_argument = parse_html(bs(r_post, 'html5lib'))
         for argument in tree_argument:
             if argument not in argument_list:
-                argument_list.extend(tree_argument)
+                argument_list.append(argument)
     return result
 
 
@@ -40,6 +46,12 @@ def parse_html(soup):
 
 def main(url):
     final_result = []
+    if '.px' in url:
+        px_page = requests.get(url=url).text
+        final_result.append({
+            url: px_page
+        })
+        return final_result
     res = send_request(url)
     main_soup = bs(res, 'html5lib')
     leaves = main_soup.find_all(attrs={'class': 'AspNet-TreeView-Leaf'})
@@ -55,16 +67,25 @@ def main(url):
         })
     return final_result
 
-if __name__ == '__main__':
-    print('-------------------- Start ------------------------')
+
+def rotate():
     initial_url = 'https://www.tilastokeskus.fi/tup/tilastotietokannat/index_en.html'
     initial_soup = bs(requests.get(url=initial_url).content, 'html5lib')
     rows = initial_soup.find(id='content').find('table').find_all('tr')
     for row in rows:
-        url = row.find_all('a')[-1]['href']
-        object = main(url=url)
-        print(len(object), object)
-    print('--------------------- The End -----------------------')
+        if row.find_all('a'):
+            url = row.find_all('a')[-1]['href']
+            print('Started to work on {}'.format(url))
+            object = main(url=url)
+            print(len(object), object)
+            print('Ended to work on {}'.format(url))
+
+if __name__ == '__main__':
+    print('---------------------- Start -----------------------')
+    url = 'http://pxnet2.stat.fi/PXWeb/pxweb/en/StatFin/'
+    object = main(url=url)
+    print(len(object), object)
+    print('---------------------- The End ----------------------')
 
 
 
